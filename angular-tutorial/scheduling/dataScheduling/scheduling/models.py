37366e34133afb8638923023ed201d359ba6dd2c
Blopausore <augustin_650@hotmail.fr>
2023-06-19 22:45:09 +0200

    Name field and data change to duration field

--- angular-tutorial/scheduling/dataScheduling/scheduling/models.py
+++ angular-tutorial/scheduling/dataScheduling/scheduling/models.py
@@ -3,8 +3,9 @@ from django.db import models
 from django.utils import timezone
 
 class Task(models.Model):
+    name = models.CharField(max_length=200, default="")
     description = models.CharField(max_length=200, default="")
-    duration = models.DateTimeField("task duration", default=timezone.now())
+    duration = models.DurationField("task duration")
     prerequisite = models.ManyToManyField('self', null=True, blank=True)
 
     def __str__(self) -> str:
